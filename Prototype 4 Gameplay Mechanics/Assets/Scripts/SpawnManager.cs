94d9467a0325fa7f8827479f1c591c8d67dcccb8
kalbii <mathieuwhang587@riversideunified.org>
2023-11-13 14:14:57 -0800

    1

--- Prototype 4 Gameplay Mechanics/Assets/Scripts/SpawnManager.cs
+++ Prototype 4 Gameplay Mechanics/Assets/Scripts/SpawnManager.cs
@@ -4,12 +4,24 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    private float spawnRange = 9;
+
     public GameObject enemyPrefab;
 
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(enemyPrefab, new Vector3(0, 0, 8), enemyPrefab.transform.rotation);
+        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+
+        //Instantiate(enemyPrefab, new Vector3(0, 0, 8), enemyPrefab.transform.rotation);
+    }
+
+    private Vector3 GenerateSpawnPosition()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+        return randomPos;
     }
 
     // Update is called once per frame
